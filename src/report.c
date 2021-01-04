c95c44281f8cb12c75384a51f2ae6bfb8c257412
Bengt Marten Agren <marangisto@me.com>
2021-01-04 20:16:46 +0800

    Some more administration.

--- src/report.c
+++ src/report.c
@@ -171,7 +171,7 @@ void report_feedback_message(uint8_t message_code)
 // Welcome message
 void report_init_message()
 {
-  printPgmString(PSTR("\r\nGrbl " GRBL_VERSION " ['$' for help]\r\n"));
+  printPgmString(PSTR("\r\nJird " GRBL_VERSION " ['$' for help]\r\n"));
 }
 
 // Grbl help message
